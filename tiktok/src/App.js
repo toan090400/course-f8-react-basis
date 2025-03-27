d77cdb8251dc62657475aff43f1c694ea9c28ceb
Duc Toan <toanbeo942000@gmail.com>
2025-03-26 20:25:02 -0700

    thiết lập react-router-dom

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "./routes";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        {publicRoutes.map((route, index) => {
+          const Pages = route.component;
+          return <Route key={index} path={route.path} element={<Pages />} />;
+        })}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
